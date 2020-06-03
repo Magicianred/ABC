46dd5461889b078b8815c1ade59f7530b9517caf
Choppychan <s.carmen2101@gmail.com>
2020-06-03 20:47:09 +0200

    Add route component on  App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import axios from "axios";
 
-class App extends Component{
-    
+//Component of the page
+import Header from "./components/layout/Header";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+
+//Bootstrap Grid System
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Header/>
+                <Container fluid>
+                    <Row>
+                        <Col>
+                            <Route path="/About" component={About}/>
+                            <Route path="/Home" component={Home}/>
+                        </Col>
+                    </Row>
+                </Container>
+            </Router>
+        );
+    }
 }
 
 export default App;
