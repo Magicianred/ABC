1cb4ac311cec8c0472249d7d6b925d846bcf3d10
Choppychan <s.carmen2101@gmail.com>
2020-06-03 16:14:13 +0200

    Add React Bootstrap

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Import the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(<App />, document.getElementById("root"));
