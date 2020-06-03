96a95fc3737d41a57e0bb6318425af3ee693765b
Choppychan <s.carmen2101@gmail.com>
2020-06-03 22:43:10 +0200

    Placed the form in the center of the page

--- my-app/src/components/pages/Search.js
+++ my-app/src/components/pages/Search.js
@@ -1,16 +1,32 @@
 import React from "react";
+
+//Bootstrap Form and Button
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
+
+//Bootstrap Grid System
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 
 function Search() {
     return (
         <React.Fragment>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
-                <Button variant="dark" type="submit">Submit</Button>
-            </Form>
+            <Container fluid>
+                <Row className="justify-content-md-center">
+                    <Col md="auto">
+                        <br />
+                        <br />
+                        <br />
+                        <Form inline>
+                            <FormControl sm={8} type="text" placeholder="Scrivi il nome di un libro..." className=" mr-sm-2" />
+                            <Button variant="dark" type="submit">Cerca</Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         </React.Fragment>
     )
 
