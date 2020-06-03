529a03491b73a884ff266a351f2864cf89e22a38
Choppychan <s.carmen2101@gmail.com>
2020-06-03 17:58:00 +0200

    On jsx <br> is different

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -3,8 +3,7 @@ import React from "react";
 function About() {
     return (
         <React.Fragment>
-            <h1>ABC - Ace Book Club</h1>
-            <p>Sito dedicato a tutti gli amanti dei libri!<br>
+            <p>Sito dedicato a tutti gli amanti dei libri!<br />
                 Ti permetterà la ricerca di una vasta collezione di libri e delle informazioni necessarie a scoprirne il contenuto.</p>
         </React.Fragment>
     )
