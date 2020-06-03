95017974db6e7dac028babbd3df5b6fc4f82901f
Choppychan <s.carmen2101@gmail.com>
2020-06-03 18:01:35 +0200

    Add new component Navbar on Header.js

--- my-app/src/components/layout/Header.js
+++ my-app/src/components/layout/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavBar from "./NavBar";
+
+
 
 function Header() {
     return (
-        <header className={headerStyle}>
+        <header className="headerStyle">
             <h1>ABC - Ace Book Club</h1>
-            <Link className={linkStyle} to="/">Home</Link> | <Link className={linkStyle} to="/about">About</Link>
+            <NavBar />
         </header>
     )
 }
