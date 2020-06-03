d1688f1cda7a3f97df0d46be4a4de5c45da870e6
Choppychan <s.carmen2101@gmail.com>
2020-06-03 22:01:37 +0200

    Add new font on the header page

--- my-app/src/components/layout/Header.js
+++ my-app/src/components/layout/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 //Bootstrap Navbar
 import Navbar from "react-bootstrap/Navbar";
+
 //Component NavBar
 import NavBar from "./NavBar";
 
 function Header() {
     return (
         <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="/Home">ABC - Ace Book Club</Navbar.Brand>
+            <Navbar.Brand href="/Home" className="brandfont">Ace Book Club</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <NavBar />
         </Navbar>
