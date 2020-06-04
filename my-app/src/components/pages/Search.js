aed6e289d1949c66db9620373c77ab7bbb93f6ab
Choppychan <s.carmen2101@gmail.com>
2020-06-04 20:41:09 +0200

    Create first search component

--- my-app/src/components/pages/Search.js
+++ my-app/src/components/pages/Search.js
@@ -1,30 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 //Bootstrap Form and Button
 import Button from "react-bootstrap/Button";
-import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
 
 //Bootstrap Grid System
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
-
 function Search() {
+
+    const [book, setBook] = useState("");
+    const [result, setResult] = useState([]);
+    const apiKey = useState(process.env.REACT_APP_API_KEY);
+
+    function handleChange(event) {
+        const book = event.target.value;
+        setBook(book);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=40")
+            .then(data => {
+                setResult(data.data.items);
+            })
+    }
     return (
         <React.Fragment>
             <Container fluid>
-                <Row className="show-grid align-items-center">
-                    <Col md={{ span: 8, offset: 2 }} xs={{span: 8, offset: 1 }}>
-                            <FormControl type="text" placeholder="Scrivi il nome di un libro..." className="mr-sm-2" />
-                    </Col>
-                    <Col>
-                            <Button variant="dark" type="submit">Cerca</Button>
-                    </Col>
-                </Row>
+                <Form onSubmit={handleSubmit}>
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Group>
+                            <Form.Control
+                                onChange={handleChange}
+                                md={{ span: 8, offset: 2 }}
+                                xs={{span: 8, offset: 1 }}
+                                type="text"
+                                placeholder="Scrivi il nome di un libro..."
+                            />
+                        </Form.Group>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Col>
+                    <Button variant="dark" type="submit">Cerca</Button>
+                        </Col>
+                    </Form.Group>
+                </Form.Row>
+                </Form>
             </Container>
+
+            {result.map(book => (
+                <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
+            ))}
         </React.Fragment>
-    )
+    );
 
 }
 
