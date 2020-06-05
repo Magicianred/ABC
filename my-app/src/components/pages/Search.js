ad9eac7c72447cb37ace0c53b32a28a34b72d219
Choppychan <s.carmen2101@gmail.com>
2020-06-05 12:39:18 +0200

    Add SearchButton on Search.js

--- my-app/src/components/pages/Search.js
+++ my-app/src/components/pages/Search.js
@@ -1,18 +1,21 @@
 import React, {useState} from "react";
 import axios from "axios";
 
-//Bootstrap Form and Button
-import Button from "react-bootstrap/Button";
+//Bootstrap Form
 import Form from "react-bootstrap/Form";
 
+//Search Button
+import SearchButton from "./element/SearchButton";
+
 //Bootstrap Grid System
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+
 
 function Search() {
 
     const [book, setBook] = useState("");
+    
     const [result, setResult] = useState([]);
     const apiKey = useState(process.env.REACT_APP_API_KEY);
 
@@ -33,22 +36,14 @@ function Search() {
             <Container fluid>
                 <Form onSubmit={handleSubmit}>
                 <Form.Row>
-                    <Form.Group as={Col}>
-                        <Form.Group>
+                    <Form.Group as={Col} md={{ span: 6, offset: 3 }} xs={{span: 8, offset: 1 }}>
                             <Form.Control
                                 onChange={handleChange}
-                                md={{ span: 8, offset: 2 }}
-                                xs={{span: 8, offset: 1 }}
                                 type="text"
                                 placeholder="Scrivi il nome di un libro..."
                             />
-                        </Form.Group>
                     </Form.Group>
-                    <Form.Group as={Row}>
-                        <Col>
-                    <Button variant="dark" type="submit"><img src="../images/search.png" alt="search" /></Button>
-                        </Col>
-                    </Form.Group>
+                    <SearchButton />
                 </Form.Row>
                 </Form>
             </Container>
