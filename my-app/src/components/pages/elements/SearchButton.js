c0e513bd38e812a45a7f2463592307688ef26808
Choppychan <s.carmen2101@gmail.com>
2020-06-05 12:38:30 +0200

    Creation of the component SearchButton

---
+++ my-app/src/components/pages/elements/SearchButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+//Bootstrap Form
+import Form from "react-bootstrap/Form";
+
+//Bootstrap Grid System
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+
+function SearchButton() {
+    return (
+        <Form.Group as={Row}>
+            <Col>
+                <Button variant="dark" type="submit"><img src="images/search.png" alt="search" /></Button>
+            </Col>
+        </Form.Group>
+    )
+
+}
+
+export default SearchButton;
