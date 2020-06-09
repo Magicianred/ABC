4e7d618acbf986c97511946af2060192662f46fc
Choppychan <s.carmen2101@gmail.com>
2020-06-09 19:52:33 +0200

    Delete Bootstrap container, row and col (i add them on the pages), add Error404 component and fix Route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,27 @@
 import React, {Component} from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import InfoPage from './components/pages/InfoPage';
+//import InfoPage from './components/pages/InfoPage';
 
-//Component of the page
+//Component made by me
 import Header from "./components/layout/Header";
 import About from "./components/pages/About";
 import Home from "./components/pages/Home";
-
-//Bootstrap Grid System
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
+import Error404 from "./components/pages/Error404";
 
 //CSS page create by me
 import "./index.css";
-
-const NoMatchRoute = () => <div>Errore 404</div>;
 
 const App = () => {
     return (
         <Router>
                 <Header />
-                <Container fluid>
-                    <Row>
-                        <Col>
-                            <Route path="/About" component={About}/>
-                            <Route path="/Home" component={Home}/>
-                        </Col>
-                    </Row>
-                </Container>
-            <Switch>
-                <Route path="/" exact component={InfoPage} />
-                <Route component={NoMatchRoute} />
-            </Switch>
+                            <Switch>
+                                <Route path="/" exact component={Home} />
+                                <Route path="/About" component={About}/>
+                                <Route path="/Home" component={Home}/>
+                                <Route path="*" component={Error404} />
+                            </Switch>
         </Router>
     );
 };
