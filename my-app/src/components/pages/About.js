68c6cafa5f7b2d8ca4a2f103a36991158e9f0392
Choppychan <s.carmen2101@gmail.com>
2020-06-09 19:17:25 +0200

    Add Bootstrap container, row and col

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-//Bootstrap Jumbotron
+//Bootstrap
 import Jumbotron from "react-bootstrap/Jumbotron";
-
-//Bootstrap Grid System
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
@@ -11,17 +9,23 @@ import Container from "react-bootstrap/Container";
 function About() {
     return (
         <React.Fragment>
-            <Jumbotron fluid className="mt-5">
             <Container fluid>
                 <Row>
                     <Col>
-                        <p>Sito dedicato a tutti gli amanti dei libri!<br />
-                            Ti permetterà la ricerca di una vasta collezione di libri e delle informazioni necessarie a scoprirne il contenuto.</p>
-                        </Col>
+                        <Jumbotron fluid className="mt-5">
+                            <Container fluid>
+                                <Row>
+                                    <Col>
+                                        <p>Sito dedicato a tutti gli amanti dei libri!<br />
+                                            Ti permetterà la ricerca di una vasta collezione di libri e delle informazioni necessarie a scoprirne il contenuto.</p>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </Jumbotron>
+                    </Col>
                 </Row>
             </Container>
-            </Jumbotron>
-            </React.Fragment>
+        </React.Fragment>
     )
 
 }
--- my-app/src/components/pages/Home.js
+++ my-app/src/components/pages/Home.js
@@ -1,16 +1,27 @@
 import React from "react";
+
+//Component made by me
 import Search from "./elements/home/Search";
 
-//Bootstrap Jumbotron
+//Bootstrap
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 
 function Home() {
     return (
         <React.Fragment>
-            <Jumbotron fluid className="mt-4">
-                        <Search />
-            </Jumbotron>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <Jumbotron fluid className="mt-4">
+                            <Search />
+                        </Jumbotron>
+                    </Col>
+                </Row>
+            </Container>
         </React.Fragment>
     )
 
