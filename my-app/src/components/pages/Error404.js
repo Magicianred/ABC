49e4f793924e61e0448c24b5738d0e7ef549d7e7
Choppychan <s.carmen2101@gmail.com>
2020-06-09 22:57:49 +0200

    Add image of Darth Vader and text of 404 component

--- my-app/src/components/pages/Error404.js
+++ my-app/src/components/pages/Error404.js
@@ -1,8 +1,32 @@
 import React from "react";
 
+//Bootstrap
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Form from "react-bootstrap/Form";
+
+
 function Error404() {
     return (
-        <div>Errore 404</div>
+
+        <Jumbotron fluid className="mt-4">
+            <Container fluid>
+                <Row className="justify-content-center">
+                    <Col xs="auto">
+                        <Image className="darthVader" src="images/DarthVader.png" alt="Darth Vader" />
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col md="auto">
+                        <h1 className="error404">ERRORE 404</h1>
+                        <h5 className="error404">Tu sottovaluti il potere del lato oscuro della forza</h5>
+                    </Col>
+                </Row>
+            </Container>
+        </Jumbotron>
     )
 
 }
