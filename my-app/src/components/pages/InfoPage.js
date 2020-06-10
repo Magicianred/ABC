d8009a34fd831c0f8f20055531aa15c760c9e8bb
Choppychan <s.carmen2101@gmail.com>
2020-06-10 18:14:25 +0200

    Set the page and the url when you click on "mostra dettagli" button

--- my-app/src/components/pages/InfoPage.js
+++ my-app/src/components/pages/InfoPage.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+//Component made by me
 import BookInfoPage from "./elements/infoPage/BookInfoPage";
+import Loading from "./elements/infoPage/Loading";
+import Error from "./elements/infoPage/Error";
 
 const InfoPage = ({ match }) => {
     const {
         params: { bookId },
     } = match;
+    const [book, setBook] = useState(null);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        let URL = ("https://www.googleapis.com/books/v1/volumes");
+        const fetchBook = async () => {
+            setLoading(true);
+            setError(false);
+            try {
+                const result = await axios.get(URL +"/" + bookId);
+                setBook(result.data);
+            } catch (error) {
+                setError(true);
+            }
+            setLoading(false);
+        };
+        // Call the API
+        fetchBook();
+    }, [bookId]);
+
 
     return (
-        <div>
-            Pagina con i dettagli del libro: <strong>{bookId}</strong>
-        </div>
+        <>
+            <Link to={`/`}>Torna indietro</Link>
+            <Loading />
+            <Error />
+            <BookInfoPage book={book} />
+        </>
     );
 };
 
