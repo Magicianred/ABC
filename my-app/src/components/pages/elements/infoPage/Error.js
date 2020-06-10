50cbde45f0571435d3f20a5ec257906288199fa2
Choppychan <s.carmen2101@gmail.com>
2020-06-10 18:10:40 +0200

    Add Error and Loading component on infoPage folder

---
+++ my-app/src/components/pages/elements/infoPage/Error.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+
+const Error = ({ error }) => {
+    return (
+        <>
+            {
+                error && (
+        <Container fluid>
+            <Row className="justify-content-md-center">
+                <Col xs="auto">
+                    <Card border="danger" style={{width: '18rem'}} className="text-center">
+                        <Card.Header as="h5">Errore</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                Si è verificato un errore durante il recupero dell'API
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+            )}
+        </>
+    );
+};
+
+export default Error;
---
+++ my-app/src/components/pages/elements/infoPage/Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+//Bootstrap
+import Spinner from "react-bootstrap/Spinner";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+const Loading = ({ loading }) =>  {
+    return (
+        <>
+        {
+            loading && (
+            <Container fluid>
+                <Row className="justify-content-center">
+                    <Col xs="auto">
+                        <Spinner animation="border" size="md" variant="secondary"/>
+                    </Col>
+                </Row>
+            </Container>
+        )
+}
+        </>
+    );
+};
+
+export default Loading;
