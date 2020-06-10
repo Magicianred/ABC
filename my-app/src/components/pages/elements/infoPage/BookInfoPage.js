e87f1cff1e08fc868a6fbc61fb671782e40707f9
Choppychan <s.carmen2101@gmail.com>
2020-06-10 18:12:09 +0200

    Add basic information page

--- my-app/src/components/pages/elements/infoPage/BookInfoPage.js
+++ my-app/src/components/pages/elements/infoPage/BookInfoPage.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
-const BookInfoPage = () => {
-    return <div>Book info page</div>;
+const BookInfoPage = ({ book }) => {
+    const createDescMarkup = description => {
+        return { __html: description };
+    };
+
+    return (
+        <>
+            {
+                book && (
+                    <section>
+                        <div>
+                            <img
+                                alt={book.volumeInfo.title}
+                                src={"http://books.google.com/books/content?id=" + book.id + "&printsec=frontcover&img=1&zoom=1&source=gbs_api"}
+                            />
+                            <div>
+                                <h3>
+                                    <strong>Titolo:</strong> {book.volumeInfo.title}
+                                </h3>
+
+                                <p>
+                                    <strong>Data di pubblicazione:</strong> {book.volumeInfo.publishedDate}
+                                </p>
+                                <p>
+                                    <strong>Editore:</strong> {book.volumeInfo.publisher}
+                                </p>
+                                <p>
+                                    <strong>Numero di pagine:</strong> {book.volumeInfo.pageCount}
+                                </p>
+                                <div
+                                    dangerouslySetInnerHTML={createDescMarkup(
+                                        book.volumeInfo.description
+                                    )}
+                                />
+                            </div>
+                        </div>
+                    </section>
+                )}
+            </>
+    );
 };
 
 export default BookInfoPage;
