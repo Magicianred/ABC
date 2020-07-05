4b7494584af4fb08772e371ee9cb8f5cbe69302e
Choppychan <s.carmen2101@gmail.com>
2020-07-05 20:23:34 +0200

    Fixed the problem when you deleted the text on the search bar and it didn't disappeared Added hooks params

--- my-app/src/components/pages/Home.js
+++ my-app/src/components/pages/Home.js
@@ -1,5 +1,5 @@
-
 import React, { useState } from "react";
+import { createBrowserHistory } from 'history';
 
 //Component made by me
 import Search from "./elements/home/Search";
@@ -17,28 +17,39 @@ const Home = ({ match }) => {
         params: { searchBook },
     } = match;
 
-    console.log(searchBook);
-
     const [book, setBook] = useState('');
-    const [searchClick, setSearchClick] = useState(false);
-    if(!book && searchBook) {
+    const [params, setParams] = useState(null);
+    const [searchClick, setSearchClick] = useState(null);
+
+    const history = createBrowserHistory();
+
+    if(params === null){
+        setParams(new URLSearchParams(history.location.search));
+    }
+
+
+    if(!book && searchBook && searchClick === null) {
         setBook(searchBook);
         setSearchClick(true);
     }
 
     return (
-      <>
-          <Container fluid>
-              <Row>
-                  <Col>
-                      <Jumbotron fluid className="mt-4">
-                          <Search searchBook={[book, setBook]} searchClick={[searchClick, setSearchClick]}/>
+        <>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <Jumbotron fluid className="mt-4">
+                            <Search
+                                searchBook={[book, setBook]}
+                                searchClick={[searchClick, setSearchClick]}
+                                searchParams={[params, setParams]}
+                            />
 
-                      </Jumbotron>
-                  </Col>
-              </Row>
-          </Container>
-      </>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 
 
